d06e5c707325d1924f96251846904e5fe4f99c05
rungo6 <t24331279@gmail.com>
2022-06-26 14:36:52 +0800

    feat: fix user profile bug

--- models/restaurantlist.js
+++ models/restaurantlist.js
@@ -2,15 +2,18 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const restaurantSchema = new Schema({
+  id: { type: String, required: false },
   name: { type: String, required: true },
-  name_en: { type: String, required: true },
+  name_en: { type: String, required: false },
   category: { type: String, required: true },
   image: { type: String, required: true },
   location: { type: String, required: true },
   phone: { type: String, required: true },
   google_map: { type: String, required: true },
-  rating: { type: Number, required: true },
+  rating: { type: String, required: true },
   description: { type: String, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', index: true, required: true }
 })
+
 
 module.exports = mongoose.model("Restaurant", restaurantSchema)
